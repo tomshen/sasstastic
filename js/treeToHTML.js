1789cc6f8224cdddbce3fe58ca5fcead885698da
Alexandra L Johnson <alexandraj.777@gmail.com>
2013-09-14 17:06:09 -0400

    Value types on tags

--- js/treeToHTML.js
+++ js/treeToHTML.js
@@ -1,6 +1,26 @@
 ﻿var propertiesKey = "properties";
 var indentation = "    ";
 
+function typeOfValue(value) {
+  if ($.inArray(value, colors) > 0) {
+    return "color";
+  }
+
+  if (value.match(/^#/) || value.match(/^rgb\(/)) {
+    return "color";
+  }
+
+  var maybeType = value.match(/(px|em|rem|%)$/);
+  if ($.isArray(maybeType)) {
+    if (maybeType[0] === "%") {
+      return "percentage";
+    }
+    return maybeType[0];
+  };
+
+  return "other";
+}
+
 function leafToHTML(leaf) {
   var result = [];
 
@@ -9,7 +29,7 @@ function leafToHTML(leaf) {
     var property = line.substring(0, line.indexOf(":"));
     var newLine = property + ":";
     $.each(values, function(idx, value) {
-      newLine += "<span class = '" + property + " " + value + "'>" + value + "</span>";
+      newLine += "<span class = '" + property + " " + value + " " + typeOfValue(value) + "'>" + value + "</span>";
     });
 
     result.push(newLine + ";<br>")
@@ -51,3 +71,144 @@ function treeToHTML(tree) {
   });
   return html;
 }
+
+var colors = ['AliceBlue',
+'AntiqueWhite',
+'Aqua',
+'Aquamarine',
+'Azure',
+'Beige',
+'Bisque',
+'Black',
+'BlanchedAlmond',
+'Blue',
+'BlueViolet',
+'Brown',
+'BurlyWood',
+'CadetBlue',
+'Chartreuse',
+'Chocolate',
+'Coral',
+'CornflowerBlue',
+'Cornsilk',
+'Crimson',
+'Cyan',
+'DarkBlue',
+'DarkCyan',
+'DarkGoldenRod',
+'DarkGray',
+'DarkGreen',
+'DarkKhaki',
+'DarkMagenta',
+'DarkOliveGreen',
+'DarkOrange',
+'DarkOrchid',
+'DarkRed',
+'DarkSalmon',
+'DarkSeaGreen',
+'DarkSlateBlue',
+'DarkSlateGray',
+'DarkTurquoise',
+'DarkViolet',
+'DeepPink',
+'DeepSkyBlue',
+'DimGray',
+'DodgerBlue',
+'FireBrick',
+'FloralWhite',
+'ForestGreen',
+'Fuchsia',
+'Gainsboro',
+'GhostWhite',
+'Gold',
+'GoldenRod',
+'Gray',
+'Green',
+'GreenYellow',
+'HoneyDew',
+'HotPink',
+'IndianRed',
+'Indigo',
+'Ivory',
+'Khaki',
+'Lavender',
+'LavenderBlush',
+'LawnGreen',
+'LemonChiffon',
+'LightBlue',
+'LightCoral',
+'LightCyan',
+'LightGoldenRodYellow',
+'LightGray',
+'LightGreen',
+'LightPink',
+'LightSalmon',
+'LightSeaGreen',
+'LightSkyBlue',
+'LightSlateGray',
+'LightSteelBlue',
+'LightYellow',
+'Lime',
+'LimeGreen',
+'Linen',
+'Magenta',
+'Maroon',
+'MediumAquaMarine',
+'MediumBlue',
+'MediumOrchid',
+'MediumPurple',
+'MediumSeaGreen',
+'MediumSlateBlue',
+'MediumSpringGreen',
+'MediumTurquoise',
+'MediumVioletRed',
+'MidnightBlue',
+'MintCream',
+'MistyRose',
+'Moccasin',
+'NavajoWhite',
+'Navy',
+'OldLace',
+'Olive',
+'OliveDrab',
+'Orange',
+'OrangeRed',
+'Orchid',
+'PaleGoldenRod',
+'PaleGreen',
+'PaleTurquoise',
+'PaleVioletRed',
+'PapayaWhip',
+'PeachPuff',
+'Peru',
+'Pink',
+'Plum',
+'PowderBlue',
+'Purple',
+'Red',
+'RosyBrown',
+'RoyalBlue',
+'SaddleBrown',
+'Salmon',
+'SandyBrown',
+'SeaGreen',
+'SeaShell',
+'Sienna',
+'Silver',
+'SkyBlue',
+'SlateBlue',
+'SlateGray',
+'Snow',
+'SpringGreen',
+'SteelBlue',
+'Tan',
+'Teal',
+'Thistle',
+'Tomato',
+'Turquoise',
+'Violet',
+'Wheat',
+'White',
+'WhiteSmoke',
+'Yellow',
+'YellowGreen']
