46213c6c89e64a989a3a9ae48de7b1ffae0f3240
Taylor Poulos <poulos.taylor.w@gmail.com>
2013-09-14 18:33:28 -0400

    added syntax highlight

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
-$(document).ready({
+$(document).ready(function() {
 	$('#css-entry-textarea').tabby({tabString:'  '});
 
+
+
+	$('#instructions-content').slideUp(0);
+	$('#instructions-toggle').click(function() {
+		$('#instructions-content').slideToggle();
+	})
 });
