dec70066f3c95ef1b26ff6e0c08ec284f0841660
Taylor Poulos <poulos.taylor.w@gmail.com>
2013-09-15 01:44:50 -0400

    hooked up code

--- js/app.js
+++ js/app.js
@@ -18,6 +18,9 @@ $(document).ready(function() {
   //Hide everything, and then show the first box
   hideEverythingImmediately();
   showEntry();
+
+  //when they click to submit the css, hide the
+  //submit area and show the editor
   $('#css-submit-button').click(function() {
     tree = parseSCSS($('#css-entry-textarea').val());
     treeHTML = treeToHTML(tree);
@@ -25,6 +28,15 @@ $(document).ready(function() {
     $(".css-container").html(treeHTML);
     showEditor();
   });
+
+
+  $("#edited-css-submit-button").click(function() {
+
+    text = $(".css-container").text();
+
+    $("#css-output-textarea").text(text);
+    showOutput();
+  }) 
 });
 
 
@@ -32,6 +44,7 @@ function hideEverythingImmediately() {
   $(".css-editor").fadeOut(0);
   $(".css-output").fadeOut(0);
   $(".css-entry").fadeOut(0);
+  $(".error").slideUp(0);
 }
 
 function showEntry() {
@@ -42,12 +55,35 @@ function showEditor() {
   $(".css-entry").fadeOut(500);
   setTimeout(function() {
     $(".css-editor").fadeIn();
-  }, 500)
+  }, 502)
 }
 
 function showOutput() {
   $(".css-editor").fadeOut(500);
   setTimeout(function() {
     $(".css-output").fadeIn();
-  }, 500)
+  }, 502)
+}
+
+function makeError(err) {
+  $("#variable-name-errors").text(err);
+}
+
+
+function getVariableName() {
+  var name = $("#variable-name-input").val();
+
+  if(name[0] !== "$") {
+    makeError("Variable names must start with the dollar sign ($)");
+    return false;
+  }
+
+  if(name.search(/\s/) !== -1) {
+    makeError("Variable names must not contain spaces")
+    return false;
+  }
+  if(name === "") {
+    makeError("Enter variable name")
+    return false;
+  }
 }
