9bfa04c1c8a3d64a32c9af2fc6712ce444dcb207
Tom Shen <tom@tomshen.me>
2013-09-14 21:26:22 -0400

    Add parser

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+var tree = {};
+
 $(document).ready(function() {
 	$('#css-entry-textarea').tabby({tabString:'  '});
 
@@ -8,5 +10,9 @@ $(document).ready(function() {
 	$('#instructions-toggle').click(function() {
 		$('#instructions-toggle').toggleClass("extended")
 		$('#instructions-content').slideToggle();
-	})
+	});
+
+  $('#css-submit-button').click(function() {
+    tree = parseSCSS($('#css-entry-textarea').value());
+  });
 });
