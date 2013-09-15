a9ec6ffd97d729b0d5a8f32dc91ffcf37ab0b0ee
Alexandra L Johnson <alexandraj.777@gmail.com>
2013-09-15 02:03:27 -0400

    Highlighting

--- js/updateCSS.js
+++ js/updateCSS.js
@@ -1,9 +1,27 @@
-﻿// Converts all active instances of value to use variableName
+﻿$(document).ready(function() {
+  $.delegate(".active-type", "click", function() {
+    var highlighted = "highlighted";
+    if ($(this).hasClass(highlighted)) {
+      $(this).removeClass(highlighted);
+    } else {
+      $(this).addClass(highlighted);
+    }
+  });
+});
+
+
+// Converts all active instances of value to use variableName
 function valueToVariable(value, variableName) {
-  $(".active-type.highlighted")
+  var highlighted = $(".active-type.highlighted");
+  highlighted
     .removeClass(value)
     .removeClass("highlighted")
     .addClass("variable")
     .text(variableName);
   $(".active-type").removeClass("active-type");
+  return highlighted.length();
+}
+
+function highLightAllOfValue(value) {
+  $("." + value).addClass("active-type").addClass("highlighted");
 }
